041ce378339b50113e84d817604d1181cc2f82d3
moha_amini <mhaminii18@gmail.com>
2024-08-28 22:43:55 +0330

    feat(auth): add User Model

---
+++ InstagramClone.API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
