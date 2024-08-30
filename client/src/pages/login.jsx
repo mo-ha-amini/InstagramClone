62ede42efd4388cf683c291aa1e73f401642a8fe
moha_amini <mhaminii18@gmail.com>
2024-08-30 14:46:00 +0330

    feat: login validation, form, and calling api added.

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,70 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
-// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useSelector, useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { ToastContainer, toast } from 'react-toastify'
+
+import { login } from '../features/auth/authActions'
+
+
+const schema = yup
+  .object({
+    username: yup.string().required('Username is Required.'),
+    password: yup.string().required('Password is Required.'),
+  })
+  .required()
+
 
 function Login() {
-  const navigate = useNavigate();
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const isInvalid = password === '' || emailAddress === '';
-  // const auth = getAuth();
-  const [loading, setLoading] = useState(false);
 
-  // const handleLogin = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     await signInWithEmailAndPassword(auth, emailAddress, password);
-  //     // await updateUserStatus(auth.currentUser.uid, true);
-  //     navigate(ROUTES.DASHBOARD);
-  //   } catch (error) {
-  //     if (error) setLoading(false);
-  //     if (error.code === 'auth/user-not-found') {
-  //       setError("User doesn't exist. Please Sign Up.");
-  //       setEmailAddress('');
-  //       setPassword('');
-  //     } else if (error.code === 'auth/wrong-password') {
-  //       setError('Invalid Password. Please Try Again.');
-  //       setPassword('');
-  //     } else if (error.code === 'auth/too-many-requests') {
-  //       setError(
-  //         'Access to this account has been temporarily disabled due to many failed login attempts. Please Reset your password or Try Again later.'
-  //       );
-  //       setEmailAddress('');
-  //       setPassword('');
-  //     } else {
-  //       setEmailAddress('');
-  //       setPassword('');
-  //       setError(error.message);
-  //     }
-  //   }
-  // };
-  // const handleResetPassword = async (event) => {
-  //   event.preventDefault();
-  //   if (emailAddress === "") {
-  //     setError("Enter your Email Address first");
-  //   } else {
-  //     try {
-  //       await sendPasswordResetEmail(auth, emailAddress);
-  //       setError(
-  //         "Password Reset Link sent successfully. Please check your email. If not found in Inbox, check your Spam folder"
-  //       );
-  //     } catch (error) {
-  //       if(error.code === "auth/invalid-email"){
-  //         setError("Enter a valid Email Address.")
-  //       }else if(error.code === "auth/user-not-found"){
-  //         setError("User Not Found. Please Sign Up.")
-  //       }
-  //        else{
-  //         setError(error.message);
-  //       }
-  //     }
-  //   }
-  //};
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loginLoading, user, loginError, LoginSuccess, } = useSelector(
+      (state) => state.auth
+    )
+
+  const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'all',
+    })
+
+    const onSubmit = (data) => {
+        dispatch(login(data))
+    }
+
   useEffect(() => {
     document.title = 'Login - Instagram';
   }, []);
+
+  useEffect(() => {
+    if (!loginLoading && user && LoginSuccess) {
+        toast.success(`Login Success`, {
+            position: 'top-right',
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+            rtl: true,
+        })
+        navigate('/')
+    }
+    if (!loginLoading && (loginError)) {
+      toast.error(`Invalid username or password`, {
+          position: 'top-right',
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+          rtl: true,
+      })
+  }
+}, [loginLoading, loginError, user , LoginSuccess])
+
   return (
     <div className="grid">
       <div>
@@ -75,43 +84,47 @@ function Login() {
           <h1 className="flex justify-center text-center ">
             <p className="mb-8 text-[22px] font-semibold text-gray-400 ">Login To Your Account.</p>
           </h1>
-          {error && <p className="error-text">{error}</p>}
-          <form method="POST">
+          {loginError && <p className="error-text">{loginError}</p>}
+          <form onSubmit={handleSubmit(onSubmit)}>
             <input
-              type="email"
-              autoComplete="email"
+              type="text"
+              autoComplete="text"
               required
-              value={emailAddress}
-              aria-label="Enter Your Email Address"
-              placeholder="Email Address"
+              aria-label="Enter Your Username"
+              placeholder="Username"
               className="input"
-              onChange={({ target }) => {
-                setEmailAddress(target.value), setError('');
-              }}
+              {...register('username')}
             />
+
+            {errors.username && (
+                    <p className=" mt-2  text-danger-500 block text-sm">
+                        {errors.username.message}
+                    </p>
+            )}
             <input
-              type="password"
+               type="password"
+               {...register('password')}
+
               autoComplete="current-password"
               required
               aria-label="Enter Your Password"
               placeholder="Password"
-              value={password}
               className="input"
-              onChange={({ target }) => {
-                setPassword(target.value), setError('');
-              }}
             />
+            {errors.password && (
+                    <p className=" mt-2  text-danger-500 block text-sm">
+                        {errors.password.message}
+                    </p>
+            )}
             <button
-              className={`submit ${isInvalid && ' bg-opacity-40'}`}
-              disabled={isInvalid}
+              className={`submit`}
+              disabled={errors.username || errors.password}
               type="submit"
-              onClick={() => setLoading(true)}
             >
-              {loading ? 'Logging In' : 'Log In'}
+              {loginLoading ? 'Logging In' : 'Log In'}
             </button>
             <h1 className="flex justify-center text-center ">
               <button
-                // onClick={handleResetPassword}
                 className="my-3 font-semibold text-blue-900 decoration-inherit"
               >
                 Forgot Password?
